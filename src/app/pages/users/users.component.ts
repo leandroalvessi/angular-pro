ae710f1f3665f081cb65930131a54bd909ec262f
Leandro Alves da Silva <leandroalvespro@icloud.com>
2025-07-06 20:14:06 -0300

    #03 - User-card

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserCardComponent } from "../../components/user-card/user-card.component";
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-users',
-  imports: [RouterLink],
+  imports: [UserCardComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  users: string[] = ["Leandro", "Marcella", "Fernanda", "Aparecida"]
+ // users: string[] = ["Leandro", "Marcella", "Fernanda", "Aparecida"]
+ user: User[] = [
+  {
+    nome: "Leandro",
+    idade: 39
+  },
+  {
+    nome: "Fernanda",
+    idade: 34
+  },
+  {
+    nome: "Marcella",
+    idade: 4
+  },
+  {
+    nome: "Aparecida",
+    idade:60
+  }
+ ]
 }
