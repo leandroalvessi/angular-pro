6ad069fdcc0c24b2b68dbf21a2b745eee5092747
Leandro Alves da Silva <leandroalvespro@icloud.com>
2025-07-06 12:18:02 -0300

    #02 - user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "users/list", component: UsersComponent,}
+    {path: "users/list", component: UsersComponent},
+    {path: "users/details", component: UserComponent},
 ];
